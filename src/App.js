2c542f507e11c22aaa2a0626389432dba9a17a53
essisuomalainen <55558262+essisuomalainen@users.noreply.github.com>
2020-08-31 22:14:42 +0200

    added a loading screen design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import './App.css';
+import style from './App.module.css';
 import Header from './components/Header/Header';
+import LoadingScreen from './components/LoadingScreen/LoadingScreen';
 import Others from './components/Others/Others';
 import Yourself from './components/Yourself/Yourself';
 import Environment from './components/Environment/Environment';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Others />
-      <Environment />
-      <Yourself />
+    <div className={style.App}>
+      <LoadingScreen />
       
     </div>
   );
-}
-
-export default App;
+};
--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import style from './Loadingscreen.module.css';
+import style from './LoadingScreen.module.css';
 
 const LoadingScreen = () => {
     return (
         <div className={style.LoadingScreenWrapper}>
-            
+            <img
+            className={style.logo}
+            id="logo"
+            src={require("../../assets/LOGO.svg")}
+            alt="logo"
+      />
+            <h2 className={style.text}>ACTS OF KINDNESS</h2>
 
         </div>
     )
