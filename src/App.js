5c513fdf0fb5ceb8005f3bbfa23ee3c9253fd2f8
essisuomalainen <55558262+essisuomalainen@users.noreply.github.com>
2020-09-07 11:37:07 +0200

    added a event listener so the user can open the random act of kindness

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 import style from './App.module.css';
 import Header from './components/Header/Header';
 import Act from './components/Act/Act';
+import Quote from './components/Quotes/Quote';
+
+
 
 function App() {
+
+  const [showCarousel, setShowCarousel] = useState(false);
+
+	const clickHandler = () => {
+		setShowCarousel(!showCarousel);
+  };
+  
   return (
+    
     <div className={style.app}>
       <div className={style.header}>
       <Header />
       </div>
+
       <div className={style.actionsWrapper}>
-      <Act name="Environment"/>
-      <Act name="Others"/>
-      <Act name="Myself" />
+     <div onClick={() => clickHandler()} ><Act name="Environment" /></div> 
+     <div onClick={() => clickHandler()}><Act name="Others" /></div> 
+     <div onClick={() => clickHandler()}><Act name="Myself" /></div> 
       </div>
+      <div className={style.Quote}>{showCarousel ? <Quote /> : null}</div>
     </div>
   );
 }
--- src/components/Quotes/Quote.js
+++ src/components/Quotes/Quote.js
@@ -3,7 +3,13 @@ import style from './Quote.module.css';
 
 const Quote = () => {
     return (
-
+        <div className={style.quoteBackground}>
+            <div className={style.timer}>24:01:59</div>
+        <div className={style.quoteWrapper}>
+            <h2 className={style.quoteHeader}>header</h2>
+            <p className={style.quoteText}>quote of the day</p>
+        </div>
+        </div>
     )
 }
 
