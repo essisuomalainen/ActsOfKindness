68a1745bb25c9ecbb2c7917c48ba473f21ef115a
essisuomalainen <55558262+essisuomalainen@users.noreply.github.com>
2020-09-13 18:14:24 +0200

    made the timer work,kinda

--- src/components/Quotes/Quote.js
+++ src/components/Quotes/Quote.js
@@ -4,21 +4,24 @@ import Timer from '../Timer/Timer.js';
 import API from '../API/API';
 import { useEffect } from 'react';
 
-const Quote = ({Clickhandler}) => {
 
+const Quote = ({Clickhandler}) => {
 const quoteData = API();
+
+
 
 useEffect(() => {
     quoteData.then((data) => {
         let quoteArrays = [];
         for (let item of data.quotes) {
-            quoteArrays.push(item.text);
+            quoteArrays.push(item.category);
         }
         
         setArray(quoteArrays);
         console.log(data);
     });
 }, []);
+
 
 const setArray = (array) => {
     const passArray = new Set(array);
@@ -35,7 +38,7 @@ const setArray = (array) => {
             <div className={style.timer}><Timer /></div>
         <div className={style.quoteWrapper}>
             <h2 className={style.quoteHeader}>OTHERS</h2>
-            <p className={style.quoteText}>Make today a day free of judgment - in words, thoughts and actions. The way we judge others is often the way the world will judge us. Don't let your fears or insecurities get in the way of truly getting to know someone. </p>
+            <p className={style.quoteText}>...</p>
         </div>
         </div>
     )
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,43 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import style from './Timer.module.css';
 
 
 const Timer = () => {
-  const [seconds, setSeconds] = useState(1111);
-  const [isActive, setIsActive] = useState(false);
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+  
 
-  function toggle() {
-    setIsActive(!isActive);
-  }
+  let interval = useRef();
 
- /* function reset() {
-    setSeconds(3600);
-    setIsActive(false);
-  }
-  */
 
-  useEffect(() => {
-    let interval = null;
-    if (isActive) {
+  const startTimer = () => {
+      const countDownDate = new Date('September 14 2020, 24:00:00').getTime();
+      //const startTime = '24:00:00';
       interval = setInterval(() => {
-        setSeconds(seconds => seconds - 1);
-      }, 1000);
-    } else if (!isActive && seconds !== 0) {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
+          const now  = new Date().getTime();
+          const distance = countDownDate - now;
+          
+          // const countDownDate = new Date().setTime(startTime);
 
+          const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor(((distance % (1000 * 60 * 60)) / (1000 * 60)));
+          const seconds = Math.floor(((distance % (1000 * 60)) / 1000));
+
+          if (distance < 0) {
+              // stop the timer
+            clearInterval(interval.current);
+          }  else {
+              // update the timer
+              setTimerHours(hours);
+              setTimerMinutes(minutes);
+              setTimerSeconds(seconds);
+          } 
+      }, );
+  }
+
+  // componentDidMount 
+  useEffect(() => {
+      startTimer();
+      return () => {
+          clearInterval(interval.current);
+      }
+  })
+  
   return (
-    <div className="app">
-      <div className="time">
-        {seconds}
+    <div className={style.timerWrapper}>
+      <div className={style.timer}>
+       {timerHours} :{timerMinutes} :{timerSeconds}
       </div>
-      <div className="row">
-        <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-          {isActive ? 'Pause' : 'Start'}
-        </button>
 
-      </div>
     </div>
   );
 };
